4d568ca740ef34e02b327f0ffe1d2bc4a1be41b7
snowingfox <1503401882@qq.com>
2019-10-14 06:50:34 +0000

    Update ZebraCrossing.py

--- ZebraCrossing.py
+++ ZebraCrossing.py
@@ -9,9 +9,9 @@ camera = cv2.VideoCapture(0)
 
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path , "yolo.h5")) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0
+detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5")) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0
 detector.loadModel()
-
-video_path = detector.detectObjectsFromVideo(,camera_input=camera,
+custom_objects = detector.CustomObjects(person=True, bicycle=True, motorcycle=True)
+video_path = detector.detectObjectsFromVideo(custom_objects=custom_objects,camera_input=camera,
                                 output_file_path=os.path.join(execution_path, "camera_detected_video")
                                 , frames_per_second=20, log_progress=True, minimum_percentage_probability=30)
