8673409ae78a8eb5d2dd108cd468fdd78f2b3dc0
snowingfox <1503401882@qq.com>
2019-10-14 07:03:33 +0000

    Update ZebraCrossing.py

--- ZebraCrossing.py
+++ ZebraCrossing.py
@@ -11,7 +11,9 @@ detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5")) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0
 detector.loadModel()
-custom_objects = detector.CustomObjects(person=True, bicycle=True, motorcycle=True)
-video_path = detector.detectObjectsFromVideo(custom_objects=custom_objects,camera_input=camera,
+custom_objects = detector.CustomObjects(person=True)
+detector = detector.detectObjectsFromVideo(custom_objects=custom_objects,camera_input=camera,
                                 output_file_path=os.path.join(execution_path, "camera_detected_video")
                                 , frames_per_second=20, log_progress=True, minimum_percentage_probability=30)
+for eachObject in detector:
+     print(eachObject["name"] , " : " , eachObject["percentage_probability"], " : ", eachObject["box_points"] )
